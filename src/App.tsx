eca86de2a60954fa5a2715d62974778095f1b0bd
marwan123-web <marawansalman98@gmail.com>
2023-04-25 18:08:20 +0200

    finished customhook to fetch data from axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import { useTranslation } from "react-i18next";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  const { data, loading, error } = useFetch({
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+  });
+
   const { t, i18n } = useTranslation("common");
-  return <div className="App">{t("hello") + " " + i18n.language}</div>;
+  if (error) {
+    console.log(error);
+  }
+  return (
+    <div className="App">
+      {loading && <div>Loading...</div>}
+      {data && data?.id + "" + t("hello") + " " + i18n.language}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const useFetch = ({
+  url,
+  method,
+  body,
+  params,
+}: {
+  url: string;
+  method?: string;
+  body?: any;
+  params?: any;
+}) => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const response: any = await axios({
+          method: method || "GET",
+          url: url,
+          ...(method !== "GET" && { data: body }),
+          params: params,
+        });
+        console.log(response.data);
+
+        setData(response.data);
+      } catch (err: any) {
+        setError(err);
+        console.log("ERROR:", err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
