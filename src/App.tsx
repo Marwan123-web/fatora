f78ffbc853ef37b1d9e37137f7e9b71b219d70a4
marwan123-web <marawansalman98@gmail.com>
2023-04-28 17:20:50 +0300

    remove unused axios call

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import "./App.scss";
 import { useTranslation } from "react-i18next";
-import useFetch from "./hooks/useFetch";
 import Invoice from "./components/invoice/Invoice";
 import { Routes, Route } from "react-router-dom";
 import InvoiceInfo from "./components/invoices/InvoiceInfo";
 import Invoices from "./components/invoices/Invoices";
 import NavBar from "./components/NavBar/NavBar";
 function App() {
-  const { data, loading, error } = useFetch({
-    url: "https://jsonplaceholder.typicode.com/todos/1",
-  });
-
   const { t, i18n } = useTranslation("common");
-  if (error) {
-    console.log(error);
-  }
   return (
     <div
       className="min-h-screen bg-gray-100"
