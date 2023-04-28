0bac41e55f619dc1025151378b89efa268960772
marwan123-web <marawansalman98@gmail.com>
2023-04-28 04:51:23 +0300

    remove top space

--- src/components/invoice/Invoice.tsx
+++ src/components/invoice/Invoice.tsx
@@ -26,7 +26,7 @@ const Invoice = () => {
       className="relative flex flex-col px-2 md:flex-row"
       onSubmit={reviewInvoiceHandler}
     >
-      <div className="my-6 flex-1 space-y-2  rounded-md bg-white p-4 shadow-sm sm:space-y-4 md:p-6">
+      <div className="my-6 flex-1 space-y-2  rounded-md bg-white p-4 shadow-sm  md:p-6">
         <Header />
         <h1 className="text-center text-lg font-bold">INVOICE</h1>
         <Info />
@@ -39,7 +39,7 @@ const Invoice = () => {
         </Button>
       </div>
       <div className="basis-1/4 bg-transparent">
-        <div className="sticky top-0 z-10 space-y-4 divide-y divide-gray-900/10 pb-8 md:pt-6 md:pl-4">
+        <div className="sticky top-0 z-10 divide-y divide-gray-900/10 pb-8 md:pt-6 md:pl-4">
           <Button
             classes={
               "w-full rounded-md bg-blue-500 py-2 text-sm text-white shadow-sm hover:bg-blue-600"
